3ff56a207ddad353b3e8d8b38532377504bc5536
Rana Bhattacharyya <ranab33@gmail.com>
2019-08-16 23:05:58 -0400

    roll functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Die from './Die';
+import RollDice from './RollDice';
 
 function App() {
   return (<div className="App" >
-    <Die face="five" />
-    <Die face="three" />
-    <Die face="four" />
-    <Die face="one" />
+    <RollDice />
   </div>
   );
 }
